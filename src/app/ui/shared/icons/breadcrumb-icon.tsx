f61bff58cc88747ee670ceee414ca5f0914bea3d
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2024-08-07 14:11:36 +0200

    feature: adds breadcrumbicon

---
+++ src/app/ui/shared/icons/breadcrumb-icon.tsx
@@ -0,0 +1,18 @@
+export const BreadcrumbIcon = () => {
+  return (
+    <svg
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="mr-1 inline-block h-4 w-4"
+    >
+      <path d="M1.74 3.35v8.35h12.54" stroke="currentColor"></path>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="m18.97 11.78-5.58 4.29V7.5l5.58 4.28Z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  );
+};
