932cdb6c4fc26fc909a40ab14355f65cdd7c672c
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2024-08-14 12:08:46 +0200

    refactor: updates hearticonprops to include favouritescount

--- src/app/ui/shared/icons/heart-icon.tsx
+++ src/app/ui/shared/icons/heart-icon.tsx
@@ -1,31 +1,36 @@
 interface HeartIconProps {
   onClick: () => void;
   isFilled: boolean;
+  favouritesCount?: number;
 }
 
-export const HeartIcon = ({ onClick, isFilled }: HeartIconProps) => {
+export const HeartIcon = ({
+  isFilled,
+  favouritesCount,
+  onClick,
+}: HeartIconProps) => {
   return (
-    <button onClick={onClick}>
-      <svg
-        viewBox="0 0 20 20"
-        fill={isFilled ? "red" : "none"}
-        xmlns="http://www.w3.org/2000/svg"
-        className="w-[18px]"
-        data-cy={
-          isFilled
-            ? "podcast-labels-heart-filled"
-            : "podcast-labels-heart-outlined"
-        } //change this
-      >
-        <path
-          clip-rule="evenodd"
-          d="M10 6.67c-.94-2.69-2.5-4.03-4.67-4.03-4.01 0-5.48 4.5-3.05 7.3 1.63 1.88 4.28 4.64 7.96 8.3 3.42-3.64 5.94-6.41 7.56-8.3 2.43-2.82.96-7.3-2.66-7.3-2.42 0-4.13 1.34-5.14 4.03Z"
-          fill={isFilled ? "red" : "#fff"}
-          fill-opacity="0.4"
-          stroke="currentColor"
-        ></path>
-      </svg>
-    </button>
+    <>
+      <button onClick={onClick}>
+        <svg
+          viewBox="0 0 20 20"
+          fill={isFilled ? "red" : "none"}
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-[18px]"
+        >
+          <path
+            clipRule="evenodd"
+            d="M10 6.67c-.94-2.69-2.5-4.03-4.67-4.03-4.01 0-5.48 4.5-3.05 7.3 1.63 1.88 4.28 4.64 7.96 8.3 3.42-3.64 5.94-6.41 7.56-8.3 2.43-2.82.96-7.3-2.66-7.3-2.42 0-4.13 1.34-5.14 4.03Z"
+            fill={isFilled ? "red" : "#fff"}
+            fillOpacity="0.4"
+            stroke="currentColor"
+          ></path>
+        </svg>
+      </button>
+      {favouritesCount !== undefined && favouritesCount > 0 && (
+        <span className="inline-block pl-2">{favouritesCount}</span>
+      )}
+    </>
   );
 };
 
