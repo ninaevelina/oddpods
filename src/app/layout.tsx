0ad76d53e8fa938d882ec9dea415a58ecd905cd7
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2024-08-14 12:11:33 +0200

    refactor: wraps app in favouritesprovider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "./ui/shared/header/header";
+import { FavouritesProvider } from "@/lib/contexts/favourites-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,11 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        {children}
-      </body>
-    </html>
+    <FavouritesProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Header />
+          {children}
+        </body>
+      </html>
+    </FavouritesProvider>
   );
 }
