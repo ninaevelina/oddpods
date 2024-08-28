4575e5fc718a65a27868822b93e12926f722bbc7
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2024-08-28 14:31:23 +0200

    refactor: updates podcasts-component - adds search functionality

--- src/app/ui/Podcasts.tsx
+++ src/app/ui/Podcasts.tsx
@@ -3,8 +3,11 @@ import { useEffect, useState } from "react";
 import { IApiResponse, IPodcast } from "@/models/IPodcast";
 import { getPodcasts } from "@/lib/actions";
 import Podcast from "@/app/ui/Podcast";
+import { useSearchParams } from "next/navigation";
 export const Podcasts = () => {
   const [podcasts, setPodcasts] = useState<IPodcast[]>([]);
+  const searchParams = useSearchParams();
+  const query = searchParams?.get("query")?.toLowerCase() || "";
 
   useEffect(() => {
     const getData = async () => {
@@ -15,10 +18,15 @@ export const Podcasts = () => {
     getData();
   }, []);
 
+  const searchedPodcasts = podcasts.filter((podcast) =>
+    podcast.name.toLowerCase().includes(query)
+  );
+
   return (
     <section className="max-w-screen-xl w-full bg-white py-10">
+      <p className="mb-[50px]">{searchedPodcasts.length} PODCASTS</p>
       <div className="grid grid-cols-1 gap-6 md:grid-cols-3 justify-items-center md:justify-items-start">
-        {podcasts.map((podcast) => (
+        {searchedPodcasts.map((podcast) => (
           <Podcast podcast={podcast} key={podcast.id} isListItem={true} />
         ))}
       </div>
