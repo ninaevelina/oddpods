1423aa5a0a97c608d465371a08f3a9239b403410
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2024-08-28 14:20:33 +0200

    feature: adds footer component

---
+++ src/app/ui/shared/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="mt-28 border-t border-black h-[50px] flex items-center justify-center">
+      OddPods &copy; Copyright 2024
+    </footer>
+  );
+}
