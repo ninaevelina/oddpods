b9e2982c866b12de05deef8f8d639fe0a87f9a27
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2024-08-28 14:18:48 +0200

    refactor: updates header

--- src/app/ui/shared/header/header.tsx
+++ src/app/ui/shared/header/header.tsx
@@ -2,22 +2,19 @@
 
 import Link from "next/link";
 import Search from "./search";
-import HeaderLogo from "../icons/header-logo";
-import { usePathname } from "next/navigation";
-import { HamburgerIcon } from "../icons/hamburger-icon";
+import { usePathname, useRouter } from "next/navigation";
 import HeartIcon from "../icons/heart-icon";
 import { useFavourites } from "@/lib/contexts/favourites-context";
 
 export const Header = () => {
   const pathname = usePathname();
+  const router = useRouter();
   const { favourites } = useFavourites();
+  console.log("FAVOURITES", favourites);
 
   const favouritesLength = favourites.length;
   const hasFavourites = favourites.length > 0;
 
-  const showAlert = () => {
-    window.alert("Test alert");
-  };
   return (
     <header className="sticky top-0 flex w-full flex-col items-center border-black transition-all duration-300 bg-white z-50">
       <div
@@ -25,21 +22,19 @@ export const Header = () => {
           pathname !== "/podcasts" ? "border-b border-black" : ""
         }`}
       >
-        <div className="flex w-full max-w-[56px] justify-center">
-          <button type="button">
-            <HamburgerIcon />
-          </button>
-        </div>
-        <div className="opacity-1 transition-all duration-300">
-          <Link href={"/"}>
-            <HeaderLogo />
+        <div className="flex w-full max-w-[56px] items-center">
+          <Link href={"/"} className="font-bold ml-[10px] mr-[10px] text-2xl">
+            ODDPODS
+          </Link>
+          <Link href={"/podcasts"} className="uppercase">
+            Podcasts
           </Link>
         </div>
         <div className="text-small flex h-full items-center overflow-hidden px-0 w-full max-w-[80px]">
           <HeartIcon
             isFilled={hasFavourites}
             favouritesCount={favouritesLength}
-            onClick={showAlert}
+            onClick={() => router.push("/my-favourites")}
           />
         </div>
       </div>
